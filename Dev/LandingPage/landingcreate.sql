9aa8cabf04315586f949c316bad9e638a1b36867
Tero <tero.urponen@gmail.com>
2014-01-11 17:56:29 +0200

    sql create

--- Dev/LandingPage/landingcreate.sql
+++ Dev/LandingPage/landingcreate.sql
@@ -4,7 +4,7 @@ USE `pitchdealanding`;
 --
 -- Host: localhost    Database: pitchdealanding
 -- ------------------------------------------------------
--- Server version	5.6.15
+-- Server version	5.6.14
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,32 @@ USE `pitchdealanding`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contactform`
+--
+
+DROP TABLE IF EXISTS `contactform`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contactform` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contactform`
+--
+
+LOCK TABLES `contactform` WRITE;
+/*!40000 ALTER TABLE `contactform` DISABLE KEYS */;
+INSERT INTO `contactform` VALUES (1,'Tero','tero.urponen@gmail.com','asd\r\nasd\r\nasd\r\n'),(2,'Tero','tero.urponen@gmail.com','asd\r\nasd\r\nasd\r\n');
+/*!40000 ALTER TABLE `contactform` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `subsc_emails`
@@ -30,6 +56,15 @@ CREATE TABLE `subsc_emails` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subsc_emails`
+--
+
+LOCK TABLES `subsc_emails` WRITE;
+/*!40000 ALTER TABLE `subsc_emails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subsc_emails` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -40,4 +75,4 @@ CREATE TABLE `subsc_emails` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-12-12 17:29:59
+-- Dump completed on 2014-01-11 17:56:07
